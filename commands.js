8f6ffd3c124f7c554d080e267aa12e1a500ac3f7
Glitch (hello-express) <none>
2019-09-29 01:48:56 +0000

    👍🏪 Checkpoint ./package.json:5640074/1507 ./server.js:5640074/1566 ./commands.js:5640074/154

---
+++ commands.js
@@ -0,0 +1,8 @@
+const commands = [
+  {
+    "name": "test",
+    "description": "Just a test command.",
+    "defaultResponse": "Hello World"
+  }
+]
+module.exports = commands
--- server.js
+++ server.js
@@ -16,28 +16,6 @@ client.on('ready', () => {
     });
 });
 
-client.config = require("./config.js");
 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
-});
-
-client.commands = new Discord.Collection();
-
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
 
 client.login(process.env.TOKEN);

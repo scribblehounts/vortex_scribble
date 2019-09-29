868973529339ddfb0d77df239df259d1c8080be2
Glitch (hello-express) <none>
2019-09-29 04:30:14 +0000

    🎒🐪 Checkpoint ./commands/moderation/addAlly.js:5640074/485 ./commands/info/help.js:5640074/256 ./commands/moderation/addally.js:5640074/185 ./commands/moderation/create.js:5640074/703

--- commands/moderation/create.js
+++ commands/moderation/create.js
@@ -1,20 +1,25 @@
 const Discord = require("discord.js");
 
 module.exports = {
-  name: "addAlly",
+  name: "create",
   category: "moderation",
   description: "Adds an ally",
-  usage: "<input>",
   run: async (client, message, args) => {
+    console.log("called")
     if (message.deletable) message.delete();
     
     if (args.length < 1)
       return message.reply("Nothing to say?").then(m => m.delete(5000));
     
-    const roleColor = message.guild.me.displayHexColor === "#000000" ? "ffffff" : message.guild.me.displayHexColor;
-    
-    if (args[0].toLowerCase() === "embed") {
+    if (args[0].toLowerCase() === "ally") {
       const embed = new Discord.RichEmbed()
+      .setColor("#ff4757")
+      .setDescription(args.slice(1).join(" "))
+      .setFooter("Ally")
+      .setAuthor("Ally")
+      message.channel.send(embed);
+    } else {
+      message.channel.send(args.join(" "));
     }
   }
 }

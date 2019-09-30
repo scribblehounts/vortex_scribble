68e4e378c84fb7e8ad18311928fb6df0b69cd31a
Glitch (hello-express) <none>
2019-09-30 03:37:23 +0000

    📌🌪 Checkpoint ./commands/moderation/send.js:5640074/3345

--- commands/moderation/send.js
+++ commands/moderation/send.js
@@ -6,21 +6,24 @@ module.exports = {
   description: "To send an bot message",
   run: async(client, message, args) => {
 if (message.author.bot) return;
-    
-    const filter = (reaction, user) => {
-    return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
-};
-    
-    message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-    .then(collected => {
-      message.channel.send(new Discord.RichEmbed().setTitle("Support Ticket").setDescription(`**React with the following emoji to start a ticket!**`).setColor("#ff4757"))
-      .then(function (message) {
-        message.react("✅")
-        
-        
-      })
-    })
-    
+message.react('👍').then(() => message.react('👎'));
 
+const filter = (reaction, user) => {
+	return ['👍', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
+};
+
+message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+	.then(collected => {
+		const reaction = collected.first();
+
+		if (reaction.emoji.name === '👍') {
+			message.reply('you reacted with a thumbs up.');
+		} else {
+			message.reply('you reacted with a thumbs down.');
+		}
+	})
+	.catch(collected => {
+		message.reply('you reacted with neither a thumbs up, nor a thumbs down.');
+	});
   }
 }

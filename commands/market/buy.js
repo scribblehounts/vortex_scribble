8d4f821aee000d8518f432db4e2ad6eddc169507
Glitch (hello-express) <none>
2019-09-30 00:16:26 +0000

    🌵💃 Checkpoint ./commands/market/buy.js:5640074/315

---
+++ commands/market/buy.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "buy",
+  category: "market",
+  description: "To purchase an item",
+  run: async(client, message, args) => {
+if (message.author.bot) return;
+    
+  }
+}

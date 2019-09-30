269814e46436954ce4a801071f80171dce0613cd
Glitch (hello-express) <none>
2019-09-30 00:28:20 +0000

    📈🚓 Checkpoint ./commands/market/buy.js:5640074/241

--- commands/market/buy.js
+++ commands/market/buy.js
@@ -4,6 +4,7 @@ module.exports = {
   description: "To purchase an item",
   run: async(client, message, args) => {
 if (message.author.bot) return;
-    
+    message.delete()
+    message.author.send("You have requested to purchase.")
   }
 }

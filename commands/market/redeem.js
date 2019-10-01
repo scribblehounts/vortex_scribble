ca571a4ed1029c9d59402f80d4d6ceb7d657713a
Glitch (hello-express) <none>
2019-10-01 03:54:11 +0000

    👡🚄 Checkpoint ./package.json:5640074/1784 ./commands/market/redeem.js:5640074/998

--- commands/market/redeem.js
+++ commands/market/redeem.js
@@ -1,13 +1,16 @@
 let Discord = require('discord.js');
 let FieldValue = require('firebase-admin').firestore.FieldValue;
-let roblox = require('noblox.js')
+const bloxy = require('bloxy');
+const roblox = new bloxy();
 
 module.exports = {
   name: "redeem",
   category: "market",
   description: "To redeem an item",
-  run: async(client, message, args) => {
+  run: async(client, message, args, db) => {
 if (message.author.bot) return;
-    
+    db.collection('users').doc(message.guild.id).get().then((r) => {
+      
+    }
   }
 }

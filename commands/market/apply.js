55f590f3bb9ba11001be5aa795096f0db70e5d95
Glitch (hello-express) <none>
2019-10-03 08:53:27 +0000

    🐣🗼 Checkpoint ./commands/market/apply.js:5640074/455

--- commands/market/apply.js
+++ commands/market/apply.js
@@ -14,6 +14,15 @@ if (message.author.bot) return;
         var brand = real[0].toUpperCase() + real.slice(1); 
         message.reply("Please look in your DMs!")
         message.author.send(new Discord.RichEmbed().setAuthor(`${brand}`).setDescription("What is your RBLX username?").setFooter("Type #cancel to cancel").setColor("#2ecc71"))
+        .then((newmsg) => {
+          newmsg.channel.awaitMessages(response => response.content, {
+            max:1,
+           time: 50000,
+            errors: ['time'],
+            }).then((collected) => {
+            newmsg.channel.send("${collected.first()}")
+          })
+        })
       }
     } else {
       message.channel.send(new Discord.RichEmbed().setAuthor("You must choose a rank,","https://i.imgur.com/UaHfuUX.png").setDescription("**Ally**\n**Developer** ").setFooter("Application System").setColor("#ff4757"));

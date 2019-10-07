4956fa061738c673c9ed8785c039fe2edb082283
Glitch (hello-express) <none>
2019-10-07 06:38:48 +0000

    👑😽 Checkpoint ./commands/moderation/forceverify.js:5640074/7212

--- commands/moderation/forceverify.js
+++ commands/moderation/forceverify.js
@@ -1,7 +1,9 @@
-  const Discord = require("discord.js");
+/*const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client()
 const roblox = require('noblox.js');
+
+let admin_role = guild.roles.find("name", "Mod");
 
 let FieldValue = require('firebase-admin').firestore.FieldValue;
 
@@ -12,7 +14,7 @@ module.exports = {
   run: async(client, message, args, db) => {
 if (message.author.bot) return; // Dont answer yourself.
     var args = message.content.split(/[ ]+/)
-    
+    if (message.member.has(admin_role.id)){
         const filter = m => m.content.includes('done');
 const collector = message.channel.createMessageCollector(filter, { time: 15000 });
     	var username = args[1];
@@ -53,7 +55,34 @@ const collector = message.channel.createMessageCollector(filter, { time: 15000 }
     	} else {
     		message.channel.send(new Discord.RichEmbed().setTitle("Error").setDescription(`**Please enter a username.**`).setFooter("Verification").setColor("#ff4757"))
     	}
-    	return;
-    
+    	return;*/
+
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client()
+const roblox = require('noblox.js');
+
+let FieldValue = require('firebase-admin').firestore.FieldValue;
+
+module.exports = {
+  name: "forceverify",
+  category: "moderation",
+  description: "To verify another player",
+  run: async(client,message,args,db) => {
+    if (message.author.bot) return;
+    var args = message.content.split(/[ ]+/)
+    if (message.member.has(630652005400051743)){
+      var username = args[1];
+      var tokenID = message.mentions.users.first().id
+      if (username || tokenID){
+        roblox.getIdFromUsername(username).then(id => {
+          var RealName = roblox.getUsernameFromId(id).tap(function(name){
+            message.channel.send(new Discord.RichEmbed().setTitle("Success").setDescription(`**`))
+          })
+        })
+      } else {
+        message.channel.send(new Discord.RichEmbed().setTitle("Error").setDescription(`**You must have the persons username and discord tokenID!.**`).setFooter("Verification").setColor("#ff4757"))
+      }
+    }
   }
 }

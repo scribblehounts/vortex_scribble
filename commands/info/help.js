98a114945e71f3d730751ff57e010eb364c318d6
Glitch (hello-express) <none>
2019-10-09 02:52:36 +0000

    🚜👀 Checkpoint ./commands/info/help.js:5640074/75

--- commands/info/help.js
+++ commands/info/help.js
@@ -5,6 +5,6 @@ module.exports = {
   category: "info",
   description: "To see the commands",
   run: async(client, message, args) => {
-    message.channel.send(new Discord.RichEmbed().setTitle("Commands").setDescription("**!verify**  To verify your Discord account to Roblox \n**!serverinfo** To check the servers info\n**!support** To start a support ticket").setFooter("Help").setColor("#ff4757").setAuthor("Vero","https://i.imgur.com/UaHfuUX.png"))
+    message.channel.send(new Discord.RichEmbed().setTitle("Commands").setDescription("**!verify**  To verify your Discord account to Roblox \n**!serverinfo** To check the servers info\n**!support** To start a support ticket").setFooter("Help").setColor("#ff4757").setAuthor("Oasis","https://i.imgur.com/FbTPWBk.png"))
   }
 }

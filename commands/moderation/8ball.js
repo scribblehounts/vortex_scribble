1dac48e8ee8cbbda87725f3952881228819f2896
Glitch (hello-express) <none>
2019-10-10 01:00:11 +0000

    👞⛄️ Checkpoint ./commands/moderation/8ball.js:5640074/912

---
+++ commands/moderation/8ball.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client()
+const roblox = require('noblox.js');
+
+let FieldValue = require('firebase-admin').firestore.FieldValue;
+
+module.exports = {
+  name: "purge",
+  category: "moderation",
+  description: "To purge messages",
+  run: async(client,message,args,db) => {
+    if (message.author.bot) return;
+    
+  }
+}

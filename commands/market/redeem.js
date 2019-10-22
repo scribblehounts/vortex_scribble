8ae264ca51ce236dca9f8605f507499dee619736
Glitch (hello-express) <none>
2019-10-22 06:42:13 +0000

    ✌️👘 Checkpoint ./commands/moderation/mute.js:5640074/4386 ./commands/moderation/kick.js:5640074/6746 ./commands/moderation/ban.js:5640074/5120 ./commands/market/purchase.js:5640074/120 ./commands/market/redeem.js:5640074/455

--- commands/market/redeem.js
+++ commands/market/redeem.js
@@ -1,14 +1,17 @@
-/*let Discord = require('discord.js');
+let Discord = require('discord.js');
 let FieldValue = require('firebase-admin').firestore.FieldValue;
-const bloxy = require('bloxy');
-const roblox = new bloxy();
+
+const availableProducts = ""
 
 module.exports = {
   name: "redeem",
   category: "market",
-  description: "To redeem an item",
+  description: "To redeem a purchase",
   run: async(client, message, args, db) => {
 if (message.author.bot) return;
+    var args = message.content.split(/[ ]+/)
+    var product = args[1]
+    
     message.reply("You do not own any Vero Tech!")
     }
-  }*/
+  }

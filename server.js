bfc0ef7ee42ccd943428e29998aad7d76838571a
Glitch (oasis-tech) <none>
2019-10-24 10:00:04 +0000

    👹🤘 Checkpoint ./public/index.html:5640074/40 ./server.js:5640074/51

--- server.js
+++ server.js
@@ -21,10 +21,12 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//var routes = require("./routes.js")(app, db);
+var routes = require("./routes.js")(app, db);
 
 var server = app.listen(3000, function() {
   console.log("Listening on port %s", server.address().port);

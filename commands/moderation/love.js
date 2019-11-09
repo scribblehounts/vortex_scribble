c600c3d5d71b185cf8f83991a66cb3e389047692
Glitch (oasis-tech) <none>
2019-11-09 02:52:42 +0000

    ✌️🎎 Checkpoint ./commands/moderation/kick.js:5640074/9829 ./commands/general/love.js:5640074/7638 ./commands/moderation/love.js:5640074/25

---
+++ commands/moderation/love.js
@@ -0,0 +1,28 @@
+const { RichEmbed } = require("discord.js");
+const { getMember } = require("../../functions.js");
+
+module.exports = {
+  name: "love",
+  category: "moderation",
+  description: "To love someone",
+  run: async(client,message,args) => {
+           let person = getMember(message, args[0]);
+
+        if (!person || message.author.id === person.id) {
+            person = message.guild.members
+                .filter(m => m.id !== message.author.id)
+                .random();
+        }
+
+        const love = Math.random() * 100;
+        const loveIndex = Math.floor(love / 10);
+        const loveLevel = "💖".repeat(loveIndex) + "💔".repeat(10 - loveIndex);
+
+        const embed = new RichEmbed()
+            .setColor("#ffb6c1")
+            .addField(`☁ **${person.displayName}** loves **${message.member.displayName}** this much:`,
+            `💟 ${Math.floor(love)}%\n\n${loveLevel}`);
+
+        message.channel.send(embed);
+    }
+}

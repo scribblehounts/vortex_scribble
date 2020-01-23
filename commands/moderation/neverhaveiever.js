3bbbac3df4270f646fc43cb2682053822ff63de6
Glitch (oasis-tech) <none>
2020-01-23 04:01:13 +0000

    🏠❤️ Checkpoint ./commands/moderation/neverhaveiever:5640074/3487 ./commands/moderation/neverhaveiever.js:5640074/3614

---
+++ commands/moderation/neverhaveiever.js
@@ -0,0 +1,12 @@
+var facts = [
+  "Your facts", "...", "..."];
+
+module.exports = {
+  name: "neverhaveiever",
+  category: "moderation",
+  description: "weirdo custom game",
+  run: async(client,message,args) => {
+ var fact = Math.floor(Math.random() * facts.length);
+    
+    }
+}

503b67345315f8e9ac98f21d964298578c12bac5
Glitch (oasis-tech) <none>
2020-01-27 01:01:06 +0000

    🎟👓 Checkpoint ./commands/info/help.js:5640074/190

--- commands/info/help.js
+++ commands/info/help.js
@@ -5,6 +5,6 @@ module.exports = {
   category: "info",
   description: "To see the commands",
   run: async(client, message, args) => {
-    message.channel.send(new Discord.RichEmbed().setTitle("Commands").setDescription("**!verify**  To verify your Discord account to Roblox \n**!serverinfo** To check the servers info\n**!support** To start a support ticket").setFooter("Help").setColor("#ff4757").setAuthor(message.guild.name,message.guild.iconURL))
+    message.channel.send(new Discord.RichEmbed().setTitle("Commands").setDescription("**Commands**\n**!verify**  To verify your Discord account to Roblox \n**!serverinfo** To check the servers info\n**!support** To start a support ticket\n\n**Fun**\n**!8ball**\n**!love**\n**!never**\n\n**Moderation**\n**!purge**\n**!forceverify**").setFooter("Help").setColor("#ff4757").setAuthor(message.guild.name,message.guild.iconURL))
   }
 }

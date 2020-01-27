ecb834ef44b996f7a4d46194929a8593c6952faa
Glitch (oasis-tech) <none>
2020-01-27 03:11:14 +0000

    🚐📈 Checkpoint ./commands/moderation/special.js:5640074/1004

---
+++ commands/moderation/special.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+module.exports = {
+  name: "special",
+  category: "moderation",
+  description: "To say a messages",
+    run: async (bot, message, args) => {
+
+    if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("You can not use this command!")
+    
+    let argsresult;
+    let mChannel = message.mentions.channels.first()
+
+    message.delete()
+    if(mChannel) {
+        argsresult = args.slice(1).join(" ")
+          var Embed = new Discord.RichEmbed()
+    .setAuthor(message.guild.name,message.guild.iconURL)
+    .setColor("#2ecc71")
+    .setDescription(argsresult)
+    .setImage()
+        mChannel.send(Embed)
+    } else {
+        argsresult = args.join(" ")
+          var Embed = new Discord.RichEmbed()
+    .setAuthor(message.guild.name,message.guild.iconURL)
+    .setColor("#2ecc71")
+    .setDescription(argsresult);
+        message.channel.send(Embed)
+    }
+
+    }
+}

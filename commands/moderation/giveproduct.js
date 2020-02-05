a0b3e7465b254bd61359cd6c48b7e21abf4c08c3
Glitch (oasis-tech) <none>
2020-02-05 04:20:12 +0000

    💗🏁 Checkpoint ./commands/moderation/giveproduct.js:5640074/9484

---
+++ commands/moderation/giveproduct.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client()
+const roblox = require('noblox.js');
+
+let FieldValue = require('firebase-admin').firestore.FieldValue;
+
+module.exports = {
+  name: "giveproduct",
+  category: "moderation",
+  description: "To verify another player",
+  run: async(client,message,args,db) => {
+    if (message.author.bot) return;
+    var args = message.content.split(/[ ]+/)
+    if (message.member.roles.some(role => role.name === 'High Ranks')) {
+      var username = args[1];
+      if (username){
+        if (args[2] === ("ife")){
+      var docRef = db.collection("users").doc(username);
+docRef.get().then(function(doc) {
+    if (doc.exists) {
+      db.collection('users').doc(`${username}`).set({ife: "owned"},{merge: true});
+
+            message.channel.send(new Discord.RichEmbed().setTitle("Success").setDescription(`**${args[1]} Has been added!**`).setFooter("Verification").setColor("#2ecc71"))
+      }
+
+})
+        }
+      }
+  }
+  }
+}

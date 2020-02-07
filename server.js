2644bacaaa35b39cb34dd24867ed40c3b39723bd
Glitch (oasis-tech) <none>
2020-02-07 05:02:48 +0000

    🏅🏑 Checkpoint ./server.js:5640074/388

--- server.js
+++ server.js
@@ -50,6 +50,18 @@ var server = app.listen(3000, function() {
 });
 
 client.on("ready", () => {
+  const channel = client.channels.get("671261326483390464");
+  if (!channel) return console.error("The channel does not exist!");
+  channel.join().then(connection => {
+    // Yay, it worked!
+    console.log("Successfully connected.");
+  }).catch(e => {
+    // Oh no, it errored! Let's log it to console :)
+    console.error(e);
+  });
+});
+
+client.on("ready", () => {
   console.log("Online!");
   client.user.setStatus("available");
   client.user.setPresence({

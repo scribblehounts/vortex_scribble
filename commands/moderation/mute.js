bf1757d6ff40a78e982f0256c41ed73e269c5004
Glitch (oasis-tech) <none>
2020-02-08 00:44:42 +0000

    🍁🚪 Checkpoint ./commands/moderation/userinfo.js:5640074/15198 ./colours.json:5640074/433 ./commands/moderation/mute.js:5640074/3699 ./commands/moderation/unmute.js:5640074/5993

---
+++ commands/moderation/mute.js
@@ -0,0 +1,62 @@
+const Discord = require("discord.js")
+
+module.exports = {
+  name: "mute",
+  category: "moderation",
+  description: "weirdo custom game",
+  run: async(client,message,args) => {
+if(!message.member.hasPermission("MANAGE_ROLES") || !message.guild.owner) return message.channel.send("You dont have permission to use this command.");
+
+if(!message.guild.me.hasPermission(["MANAGE_ROLES", "ADMINISTRATOR"])) return message.channel.send("I don't have permission to add roles!")
+
+//define the reason and mutee
+let mutee = message.mentions.members.first() || message.guild.members.get(args[0]);
+if(!mutee) return message.channel.send("Please supply a user to be muted!");
+
+let reason = args.slice(1).join(" ");
+if(!reason) reason = "No reason given"
+
+//define mute role and if the mute role doesnt exist then create one
+let muterole = message.guild.roles.find(r => r.name === "Muted")
+if(!muterole) {
+    try{
+        muterole = await message.guild.createRole({
+            name: "Muted",
+            color: "#514f48",
+            permissions: []
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+            await channel.overwritePermissions(muterole, {
+                SEND_MESSAGES: false,
+                ADD_REACTIONS: false,
+                SEND_TTS_MESSAGES: false,
+                ATTACH_FILES: false,
+                SPEAK: false
+            })
+        })
+    } catch(e) {
+        console.log(e.stack);
+    }
+}
+
+//add role to the mentioned user and also send the user a dm explaing where and why they were muted
+mutee.addRole(muterole.id).then(() => {
+    message.delete()
+    mutee.send(`Hello, you have been in ${message.guild.name} for: ${reason}`).catch(err => console.log(err))
+    message.channel.send(`${mutee.user.username} was successfully muted.`)
+})
+
+//send an embed to the modlogs channel
+let embed = new Discord.RichEmbed()
+.setColor("#ff4757")
+.setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
+.addField("Moderation:", "mute")
+.addField("Mutee:", mutee.user.username)
+.addField("Moderator:", message.author.username)
+.addField("Reason:", reason)
+.addField("Date:", message.createdAt.toLocaleString())
+
+let sChannel = message.guild.channels.find(c => c.name === "logs")
+sChannel.send(embed)
+  }
+}

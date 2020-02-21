2161e67f817a9396add3de9f984c1c42a7f812a8
Glitch (oasis-tech) <none>
2020-02-21 00:28:18 +0000

    📇🍼 Checkpoint ./commands/moderation/find.js:5640074/3729

--- commands/moderation/find.js
+++ commands/moderation/find.js
@@ -6,39 +6,30 @@ const roblox = require('noblox.js');
 let FieldValue = require('firebase-admin').firestore.FieldValue;
 
 module.exports = {
-  name: "ifeowned",
+  name: "find",
   category: "moderation",
   description: "To verify another player",
   run: async(client,message,args,db) => {
     if (message.author.bot) return;
     var args = message.content.split(/[ ]+/)
+if (message.member.roles.some(role => role.name === 'Mod')){
+  var username = args[1];
+  var second = args[2];
+  
+  if (username){
+    if (second === "ife"){
+            var docRef = db.collection("users").doc(username);
+docRef.get().then(function(doc) {
+  if (doc.exists){
+      message.channel.send(new Discord.RichEmbed().setTitle("Success").setDescription(`**${username} owns ${second}**`).setFooter("Product System").setColor("#2ecc71"))
+  } else {
     
-    db.collection("users").where("discord" || "tokenID", "==", message.author.id).get().then(function(querySnapShot){
-      querySnapShot.forEach(function(doc){
-        var data = doc.data();
-        if (data.tokenID){
-if (data.ife){
-                  message.channel.send(new Discord.RichEmbed().setTitle(`**${doc.data().tokenID}**`).setDescription("you own it yes").setFooter("Product System").setColor("#2ecc71"))
-}                   else {
-                    message.channel.send(new Discord.RichEmbed().setTitle(`**${doc.data().discord}**`).setDescription("you do not own it nuu").setFooter("Product System").setColor("#2ecc71"))
-                  }
-        }
-                if (data.discord){
-                  if (data.ife){
-                  message.channel.send(new Discord.RichEmbed().setTitle(`**${doc.data().discord}**`).setDescription("you own it yes").setFooter("Product System").setColor("#2ecc71"))
-                  } else {
-                    message.channel.send(new Discord.RichEmbed().setTitle(`**${doc.data().discord}**`).setDescription("you do not own it nuu").setFooter("Product System").setColor("#2ecc71"))
-                  }
-        }
-      })
-    })
-
-    
-    
-
-      
-
-
-
+    message.channel.send(new Discord.RichEmbed().setTitle("Success").setDescription(`**${username} doesn't own ${second}**`).setFooter("Product System").setColor("#2ecc71"))
   }
+})
+    }
+  }
+}
+    
+    }
 }

9270318fa57fe3668790a83dd3cdbfe2b9e207e1
Glitch (brick-theater-mw39xi4x98) <none>
2020-04-05 09:07:44 +0000

    🛫🌗 Checkpoint ./commands/moderation/warn.js:5640074/1482 ./warns.json:5640074/6 ./commands/moderation/profile.js:5640074/1878

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -1,22 +1,40 @@
 const Discord = require("discord.js")
 
+const bot = new Discord.Client();
+
+const fs = require ('fs');
+
+const file = JSON.parse(fs.readFileSync("./././warns.json", "utf8"));
+
 module.exports = {
   name: "warn",
   category: "moderation",
   description: "weirdo custom game",
   run: async(client,message,args) => {
- let uEmbed = new Discord.RichEmbed()
-    .setColor("#ff4757")
-    .setTitle("Server Info")
-    .setThumbnail(message.guild.iconURL)
-    .setAuthor(`${message.author.username} Info`, message.author.displayAvatarURL)
-    .addField("**Username:**", `${message.author.username}`, true)
-    .addField("**Discriminator:**", `${message.author.discriminator}`, true)
-    .addField("**ID:**", `${message.author.id}`, true)
-    .addField("**Status:**", `${message.author.presence.status}`, true)
-    .addField("**Created At:**", `${message.author.createdAt}`, true)
-    .setFooter(`Bot | Footer`, client.user.displayAvatarURL);
 
-    message.channel.send({embed: uEmbed});
+if (!message.member.hasPermission("DEAFEN_MEMBERS")) return message.channel.send("Sorry. Seems you dont have the correct permissions to run this command!");
+
+
+if (!file[message.mentions.members.first().id]) {
+  file[message.mentions.members.first().id] = {
+    warns: 0
+  }
+
+};
+
+
+file[message.mentions.members.first().id] = {
+  warns: file[message.mentions.members.first().id].warns + 1
+}
+
+
+fs.writeFile("./warns.json", JSON.stringify(file), (err) => {
+  if (err) console.error(err);
+
+  console.log("Added a warn to that bitchass kid!")
+});
+
+message.channel.send("Added a warn to the user " + message.mentions.members.first().user.tag);
+
   }
 }

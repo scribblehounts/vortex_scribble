ec1584decadbdaf74abca20eba25214e19dffefd
Glitch (brick-theater-mw39xi4x98) <none>
2020-04-08 11:59:12 +0000

    🚆👶 Checkpoint ./commands/fun/rolldice.js:5640074/7458 ./commands/moderation/lockdown.js:5640074/5759 ./package.json:5640074/2010

---
+++ commands/fun/rolldice.js
@@ -0,0 +1,27 @@
+const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
+const { promptMessage } = require("../../functions.js");
+const Discord = require('discord.js')
+
+module.exports = {
+  name: "rolldice",
+  category: "moderation",
+  description: "To kick a member",
+  run: async(client,message,args) => {
+  try {
+
+  let replies = ["1", "2", "3", "4", "5", "6", "Null"]
+
+  let result = Math.floor((Math.random() * replies.length))
+
+  let ballembed = new Discord.RichEmbed()
+  .setAuthor(message.author.tag)
+  .setColor("#000000")
+  .addField("Your Dice number is...", replies[result])
+
+  message.channel.send(ballembed);
+    } catch(err) {
+      message.channel.send(`Whoops, We got a error right now! This error has been reported to Support center!`)
+    }
+    }
+}

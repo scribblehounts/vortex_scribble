3949283ba196d7d1b047328d15cb4727dcc99ea4
Glitch (brick-theater-mw39xi4x98) <none>
2020-04-09 06:07:17 +0000

    🎍💷 Checkpoint ./commands/moderation/special.js:5640074/722

--- commands/moderation/special.js
+++ commands/moderation/special.js
@@ -7,14 +7,7 @@ module.exports = {
     run: async (bot, message, args) => {
 
     if(!message.member.hasPermission(["MANAGE_MESSAGES", "ADMINISTRATOR"])) return message.channel.send("You can not use this command!")
-  
-
-let role = message.guild.roles.find(r => r.name == 'Customer')
-
-if (!role) return message.channel.send(`**${message.author.username}**, role not found`)
-
-message.guild.members.filter(m => !m.user.bot).forEach(member => member.addRole(role))
-message.channel.send(`**${message.author.username}**, role **${role.name}** was added to all members`)
-
+      message.channel.send(new Discord.RichEmbed().setImage("https://i.imgur.com/mbuQUjI.png").setColor("#4d91ff"))
+      
     }
 }

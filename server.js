bd4fef73a94067f3d38759742359b35646eecbc0
Glitch (brick-theater-mw39xi4x98) <none>
2020-04-19 13:46:04 +1200

    added anti spam hopefully will work?

--- server.js
+++ server.js
@@ -26,6 +26,26 @@ var bodyParser = require("body-parser");
 var app = express();
 
  client.categories = fs.readdirSync("./commands/");
+ const AntiSpam = require('discord-anti-spam');
+ const antiSpam = new AntiSpam({
+     warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
+     kickThreshold: 7, // Amount of messages sent in a row that will cause a ban.
+     banThreshold: 13, // Amount of messages sent in a row that will cause a ban.
+     maxInterval: 2000, // Amount of time (in milliseconds) in which messages are considered spam.
+     warnMessage: '{@user}, Please stop spamming.', // Message that will be sent in chat upon warning a user.
+     kickMessage: '**{user_tag}** has been kicked for spamming.', // Message that will be sent in chat upon kicking a user.
+     banMessage: '**{user_tag}** has been banned for spamming.', // Message that will be sent in chat upon banning a user.
+     maxDuplicatesWarning: 7, // Amount of duplicate messages that trigger a warning.
+     maxDuplicatesKick: 10, // Amount of duplicate messages that trigger a warning.
+     maxDuplicatesBan: 14, // Amount of duplicate messages that trigger a warning.
+     exemptPermissions: [], // Bypass users with any of these permissions.
+     ignoreBots: true, // Ignore bot messages.
+     verbose: true, // Extended Logs from module.
+     ignoredUsers: [], // Array of User IDs that get ignored.
+     // And many more options... See the documentation.
+ });
+  
+ client.on('message', (message) => antiSpam.message(message)); 
 
 client.on("guildMemberAdd", member => {
   let channel = client.channels.cache.get("671258373303566336");

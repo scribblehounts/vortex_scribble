9e4de411a0a06bef9716a10aa30ee15ad134ffc5
Glitch (brick-theater-mw39xi4x98) <none>
2020-04-29 04:01:47 +0000

    yes

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 const roblox = require("noblox.js");
-const htpts = require('https')
+const https = require('https')
 
 var randomString = function (len, bits)
 {
@@ -122,7 +122,7 @@ app.get("/checkperms", function(req,res){
   if (req.query.plr){
       var doc = db.collection("users").doc(req.query.plr);
       doc.get().then(function(doc){
-        htpts.get('http://149.28.225.161:3000/wdhuiawhpfiuhawuifhawuihfiuawhfiuhwafh/' + doc.data().discord, (resp)=>{
+        https.get('https://149.28.225.161:3000/wdhuiawhpfiuhawuifhawuihfiuawhfiuhwafh/' + doc.data().discord, (resp)=>{
           resp.on('data',(chunk)=>{
             console.log(JSON.parse(data).explanation)
           })

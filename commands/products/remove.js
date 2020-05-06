c70130f0147e82ec787ca0154753b14e9e4b1566
Glitch (brick-theater-mw39xi4x98) <none>
2020-05-06 14:57:11 +1200

    major update

---
+++ commands/products/remove.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client()
+const roblox = require('noblox.js');
+const editJsonFile = require('edit-json-file')
+let FieldValue = require('firebase-admin').firestore.FieldValue;
+
+module.exports = {
+  name: "remove",
+  category: "products",
+  description: "To verify another player",
+  run: async(client,message,args,db) => {
+    if (message.author.bot) return;
+    var args = message.content.split(/[ ]+/)
+    if (message.member.roles.cache.some(role => role.name === 'Mod')) {
+      var username = args[1];
+      if (username){
+        let file = editJsonFile('././products.json').data
+        for (var i in file){
+          console.log("correct")
+          if (!args[2] === i){
+            console.log("wrong")
+            return message.reply("invalid subjects")
+          }
+        }
+  var docRef = db.collection("users").doc(username);
+docRef.get().then(function(doc) {
+if (doc.exists) {
+  var obj = {}
+  obj[args[2]] = FieldValue.delete()
+
+  db.collection('users').doc(`${username}`).set(obj,{merge: true});
+
+        message.channel.send(new Discord.MessageEmbed().setTitle("Success").setDescription(`**${args[1]} Has been removed!**`).setFooter("Product System").setColor("#2ecc71"))
+  }
+
+})
+      }
+  }
+  }
+}

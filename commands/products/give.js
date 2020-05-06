6264731e4e6ac6140d6ba9a75e82129b79c87500
Glitch (brick-theater-mw39xi4x98) <none>
2020-05-06 14:56:52 +1200

    major update

--- commands/products/give.js
+++ commands/products/give.js
@@ -2,60 +2,42 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client()
 const roblox = require('noblox.js');
+const editJsonFile = require('edit-json-file')
 
 let FieldValue = require('firebase-admin').firestore.FieldValue;
 
 module.exports = {
   name: "give",
-  category: "products",
-  description: "To give a person ally,rubeus,staff role",
+  category: "moderation",
+  description: "To verify another player",
   run: async(client,message,args,db) => {
     if (message.author.bot) return;
     var args = message.content.split(/[ ]+/)
-if (message.member.roles.cache.some(role => role.name === 'Owner')){
-  
-  var username = message.mentions.members.first()
-  
-  if(!args[2]){
-    message.reply("subject not found! you didn't specifiy wether it was rubeus or ally or staff sir.")
-    return
-  }
-
-  if (!args[2].toLowerCase() === "ally"  || !args[2].toLowerCase() === "staff"){
-    message.reply("invalid subject!")
-    return
-  }
-
-  if (!username){
-        var docRef = db.collection("users").doc(args[1]);
-    docRef.get().then(function(doc) {
+    if (message.member.roles.cache.some(role => role.name === 'Mod')) {
+      var username = args[1];
+      if (username){
+            let file = editJsonFile('././products.json').data
+            for (var i in file){
+              console.log("correct")
+              if (!args[2] === i){
+                console.log("wrong")
+                return message.reply("invalid subjects")
+              }
+            }
+      var docRef = db.collection("users").doc(username);
+docRef.get().then(function(doc) {
     if (doc.exists) {
-      roblox.getUsernameFromId(doc.id).then(function(username){
-        db.collection('users').doc(doc.id).set({SPECIAL: `${args[2].toLowerCase()}`},{merge: true});
-        return message.channel.send(new Discord.MessageEmbed().setTitle("Success!").setDescription("Added!").setFooter("Product System").setColor("#2ecc71"))
-        })
-           
+      var obj = {}
+      obj[args[2]] = "owned"
+
+      db.collection('users').doc(`${username}`).set(obj,{merge: true});
+
+            message.channel.send(new Discord.MessageEmbed().setTitle("Success").setDescription(`**${args[1]} Has been added!**`).setFooter("Product System").setColor("#2ecc71"))
       }
 
 })
+        
+      }
   }
-
-        db.collection('users').where('discord','==',username.id).get().then(exist => {
-        if (exist.empty){
-          message.reply("user is not linked")
-          return;
-        }
-          exist.forEach(doc => {
-            roblox.getUsernameFromId(doc.id).then(function(username){
-                db.collection('users').doc(doc.id).set({SPECIAL: `${args[2].toLowerCase()}`},{merge: true});
-              return message.channel.send(new Discord.MessageEmbed().setTitle("Success!").setDescription("Added!").setFooter("Product System").setColor("#2ecc71"))
-            })
-
-            
-          })
-        })
-  
-}
-  }   
-    
+  }
 }
